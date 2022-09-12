4c1640d12fc775812d283b5efbfe1bbd65e76232
Nicole Robbins <nicole.robbins@att.net>
2022-09-11 21:01:57 -0500

    commit tree bbe9db006abf27d2219e0522d5208a717517268d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+let people = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+let fare = 5;
+
+const returnFirstTwoDrivers = function(names){
+    return names.slice(0,2);
+}
+const returnLastTwoDrivers = function(names){
+    return names.slice(2);
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier){
+    return function(fare){
+        return multiplier * fare};
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+let selectDifferentDrivers = function (names, ab){
+    if (ab === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(names);
+    }
+    else {
+        return returnLastTwoDrivers(names);
+    }
+}
+
+returnFirstTwoDrivers(people);
+returnLastTwoDrivers(people);
+selectingDrivers;
+createFareMultiplier();
+createFareDoubler();
+createFareTripler();
+fareDoubler();
+fareTripler();
+selectDifferentDrivers();
